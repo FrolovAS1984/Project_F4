ba32c4cf716153a346c5374d35c28883ad054a43
Mortius <slaider84@list.ru>
2024-06-14 10:59:19 +0300

    14.06.2024

--- Backend/BackendProject/BackendProject/settings.py
+++ Backend/BackendProject/BackendProject/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'rest_framework_swagger',
 
     'App'
 ]
--- Backend/BackendProject/BackendProject/urls.py
+++ Backend/BackendProject/BackendProject/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+
 from App.views import CategoryApiView, DishesApiView, dishes_view
 
 router = routers.DefaultRouter()
@@ -12,5 +15,16 @@ router.register(r'api/recipes', DishesApiView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api/dishes/', dishes_view)
+    path('api/dishes/', dishes_view),
+    path('openapi/', get_schema_view(
+        title="Django Rest API",
+        description="API description",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ),
+         name='swagger-ui'),
+
 ]
