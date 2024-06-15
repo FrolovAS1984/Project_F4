d04f40e0aa2398cb8dd3a13bb6b1111f08117573
Frolov_AS <slaider84@list.ru>
2024-06-15 16:10:12 +0300

    15.06.2024

--- Backend/BackendProject/App/urls.py
+++ Backend/BackendProject/App/urls.py
@@ -1,17 +1,14 @@
-
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 from .views import CategoryApiView, DishesApiView, dishes_view
 
 router = routers.DefaultRouter()
 router.register('categories', CategoryApiView)
 router.register('recipes', DishesApiView)
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -29,8 +26,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', include(router.urls)),
     path('dishes/', dishes_view),
-    path('swagger/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
-
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
--- Frontend/src/components/Dishes/Dishes.jsx
+++ Frontend/src/components/Dishes/Dishes.jsx
@@ -2,6 +2,7 @@ import {useEffect , useState} from "react";
 import axios from "axios";
 import {URL_Dishes} from "../../const/const.js";
 import {Link , useParams} from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
 
 import styles from './Dishes.module.css'
 
@@ -9,13 +10,28 @@ import styles from './Dishes.module.css'
 function Dishes() {
     const category = useParams().category;
     const [dishes, setDishes] = useState([]);
+    const [isLoading, setLoading] = useState(true);
 
-    useEffect(()=> {
-        axios.get(`${URL_Dishes}${category}`).then(res => {
+    useEffect(() => {
+          axios.get(`${URL_Dishes}${category}`).then((res) => {
             setDishes(res.data);
+            setLoading(false);
+          });
 
-        })
-    }, [dishes])
+      }, [dishes]);
+
+    if (isLoading) {
+        return (
+            <section className={styles.loader}>
+                <ClipLoader className={styles.spin}
+                    color={'#38796c'}
+                    size={70}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />
+            </section>
+        )
+    }
 
 
     return (
